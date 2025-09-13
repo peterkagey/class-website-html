9fb9a3d5a16e37df8b90383bd814730650323254
Peter_Kagey <peterkagey@gmail.com>
2025-09-12 19:39:10 -0700

    Octahedron app: less orthogonal for Scott.

--- apps/scripts/octahedral_symmetry/scene_setup.js
+++ apps/scripts/octahedral_symmetry/scene_setup.js
@@ -3,23 +3,23 @@ import * as THREE from "three";
 export const scene = new THREE.Scene();
 export const renderer = new THREE.WebGLRenderer();
 
-export const camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(5, 5, 5);
+export const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(1.5, 1.5, 1.5);
 camera.lookAt(0, 0, 0);
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const redLight = new THREE.DirectionalLight(0xffff00, 1, 100);
+const redLight = new THREE.DirectionalLight(0xffff00, 0.7, 100);
 redLight.position.set(5, 0, 0);
 camera.add(redLight);
 
-const greenLight = new THREE.DirectionalLight(0x00ffff, 1, 100);
+const greenLight = new THREE.DirectionalLight(0x00ffff, 0.7, 100);
 greenLight.position.set(0, 5, 0);
 camera.add(greenLight);
 
-const blueLight = new THREE.DirectionalLight(0xff00ff, 1, 100);
+const blueLight = new THREE.DirectionalLight(0xff00ff, 0.7, 100);
 blueLight.position.set(0, 0, 5);
 camera.add(blueLight);
 scene.add(camera)
