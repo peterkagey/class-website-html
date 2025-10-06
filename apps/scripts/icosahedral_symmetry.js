0275bbb1b4f6f7cb029303eadf077c59a053ed2d
Peter Kagey <peterkagey@gmail.com>
2025-10-06 13:30:08 -0700

    Remove apps.

--- apps/scripts/icosahedral_symmetry.js
+++
@@ -1,5 +0,0 @@
-import { setupTriangleControl } from './polyhedral_symmetry/triangle_control.js';
-import { makeAnimator } from './polyhedral_symmetry/scene_animation.js';
-
-setupTriangleControl("Icosahedral");
-makeAnimator("Icosahedral")();
